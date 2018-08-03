e98681fc53c9c9eff043df8e43f17532ff983e03
Jonathan Reeves <jonathanedwardreeves1988@gmail.com>
2018-08-03 14:26:37 -0500

    Add message to root page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Hello, World!!!"
+    render html: "Hello, World!!! Take a look around by changing adding /users or /microposts and try creating a user and micropost."
   end
 end
